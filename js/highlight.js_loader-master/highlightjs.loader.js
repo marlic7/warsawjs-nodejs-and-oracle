531d5115e7a4039a5dc91a92a3c206313a426449
marlic <marlic-git@phu.pl>
2015-11-15 13:34:22 +0100

    standard commit

--- js/highlight.js_loader-master/highlightjs.loader.js
+++ js/highlight.js_loader-master/highlightjs.loader.js
@@ -1,7 +1,7 @@
 /*! Highlight JS Loader v6.3 | MIT Licensed (C) 2015 Ali.Dbg | http://goo.gl/IRlJuI */
 ;(function(doc, replace, innerHTML) {
     "use strict";
-    doc.head.appendChild(doc.createElement("style"))[innerHTML] = 
+    doc.head.appendChild(doc.createElement("style"))[innerHTML] =
         ".hljs,.hljs *{border:0 none;direction:ltr;float:none;font-size:small;font-family:Consolas,Menlo,Liberation Mono;left:0;line-height:1.45;margin:0;outline:none;position:relative;top:0;vertical-align:top;word-wrap:normal}"+
         ".hljs{border-radius:.2em;margin:.5em auto;max-height:40em;white-space:pre}.hljs *{padding:0;display:inline}"+
         ".hjln{border-right:.1em solid;counter-reset:l;cursor:default;float:left;margin:0 1em 0 -1em;text-align:right;-moz-user-select:none;-webkit-user-select:none}"+
@@ -12,22 +12,22 @@
         for (var i = 0; i < code.length; i++) {
             var cod = code[i];
             if (cod.className.search(/(hljs|nohighlight)/) == -1) {
-                cod.addEventListener("click", function() {
-                    if (confirm("Select All?")) {
-                        var r = doc.createRange(),
-                            s = window.getSelection();
-                        s.removeAllRanges();
-                        r.setStart(this, 1);
-                        r.setEnd(this, this.childNodes.length);
-                        s.addRange(r)
-                    }
-                });
+                //cod.addEventListener("click", function() {
+                //    if (confirm("Select All?")) {
+                //        var r = doc.createRange(),
+                //            s = window.getSelection();
+                //        s.removeAllRanges();
+                //        r.setStart(this, 1);
+                //        r.setEnd(this, this.childNodes.length);
+                //        s.addRange(r)
+                //    }
+                //});
                 cod[innerHTML] = cod[innerHTML][replace](/<br[^>]*>$/mgi, "")[replace](/</g, "&lt;")[replace](/>/g, "&gt;")[replace](/"/g, "&quot;");
                 hljs.highlightBlock(cod);
                 var lines = new Array(cod[innerHTML].split(/\n/).length + 1).join('<span></span>');
                 cod[innerHTML] = '<span class="hjln">' + lines + '</span>' + cod[innerHTML];
             }
         }
-        if (typeof jQuery != "undefined" && jQuery.fn.niceScroll) jQuery(".hljs").niceScroll()
+        //if (typeof jQuery != "undefined" && jQuery.fn.niceScroll) jQuery(".hljs").niceScroll()
     })
 })(document, "replace", "innerHTML");
